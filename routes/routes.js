caf6b98d383778e85bbaf9e1ac5e29f027cee81d
mrasyadc <mrcrasyad26@gmail.com>
2023-10-26 22:47:24 +0700

    remove http 1.0

--- routes/routes.js
+++ routes/routes.js
@@ -33,7 +33,6 @@ router.get("/watering", async (req, res) => {
 });
 
 router.post("/watering", async (req, res) => {
-  req.httpVersion = "1.0";
   try {
     const { deviceId, status, createdAt } = req.body;
 
